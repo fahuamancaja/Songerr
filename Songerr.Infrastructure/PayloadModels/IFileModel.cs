76e3885585d340f796bbdcc6debe4c2966c82f0b
fahuamancaja <47445881+fahuamancaja@users.noreply.github.com>
2024-09-24 15:50:33 -0400

    Removed excess IFileModel

--- Songerr.Infrastructure/PayloadModels/IFileModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Songerr.Infrastructure.PayloadModels;
-
-public interface IFileModel
-{
-    string? FilePath { get; set; }
-    string? Author { get; set; }
-    string? Title { get; set; }
-    string? Album { get; set; }
-}
